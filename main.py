1ab83805e0674eecdc88dff053bce0497e7b705f
Mark Kamesnkikh <mark.kamenskikh@vk.com>
2025-03-12 14:30:40 +0300

    new code

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def hello():
 
 def hello_2():
     print('Огромный привет всем!')
+
+
+второй
+
+def hello():  
+    print('Привет всем!')  
+  
+  
+def hello_2():  
+    print('Огромный привет всем!')
